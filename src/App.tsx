cc1dbddc61d36c501608944fe86058d3e24ad798
Athos Henrique <athdsantos@gmail.com>
2021-10-05 19:59:37 -0300

    upando repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sedansImg from './images/icon-sedans.svg'
+import suvsImg from './images/icon-suvs.svg'
+import luxuryImg from './images/icon-luxury.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card card-sedans">
+        <img src={sedansImg} alt="Logo Sedans cars" />
+        <h1>SEDANS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem minus, earum voluptatem sit magnam nam non in distinctio amet esse blanditiis atque. Incidunt eius cumque nesciunt saepe quis dicta deserunt.</p>
+        <a href='/' className="btnLearnMore">Learn More</a>
+      </div>
+      <div className="card card-suvs">
+        <img src={suvsImg} alt="Logo SUVS cars" />
+        <h1>SUVS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem minus, earum voluptatem sit magnam nam non in distinctio amet esse blanditiis atque. Incidunt eius cumque nesciunt saepe quis dicta deserunt.</p>
+        <a href='/' className="btnLearnMore">Learn More</a>
+      </div>
+      <div className="card card-luxury">
+        <img src={luxuryImg} alt="Logo Luxury cars" />
+        <h1>LUXURY</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem minus, earum voluptatem sit magnam nam non in distinctio amet esse blanditiis atque. Incidunt eius cumque nesciunt saepe quis dicta deserunt.</p>
+        <a href='/' className="btnLearnMore">Learn More</a>
+      </div>
     </div>
   );
 }
